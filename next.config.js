77ad3d38a0c47fb21804f5c25fbb499787469290
RommelSharma23 <rommelsharma23@gmail.com>
2025-06-26 02:39:11 +0530

    v1.0.2 Add Next.js config for deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    serverComponentsExternalPackages: ['@supabase/supabase-js']
   },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig
